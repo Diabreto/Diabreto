2352308b9857ba7f6633626831fcb3d5996be78e
Joao Bernardo <jb.amaro95@gmail.com>
2017-02-17 18:45:10 +0000

    Adds Main Home and Profile view controllers and views

---
+++ Diabreto/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Diabreto
+//
+//  Created by Tiago Botelho on 17/02/2017.
+//  Copyright © 2017 Diabreto. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- Diabreto/ProfileViewController.swift
+++ Diabreto/ProfileViewController.swift
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ProfileViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
