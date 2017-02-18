0a23c38f9706d8cd4dbb5851e74b330a7c73ab3b
Joao Bernardo <jb.amaro95@gmail.com>
2017-02-18 15:50:42 +0000

    Adds graph view

--- Diabreto/HomeViewController.swift
+++ Diabreto/HomeViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import ScrollableGraphView
 
 class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var graphView: ScrollableGraphView!
     
     var glucose = [102,205,70,102,205,70,102,205,70]
     var carbs = [15,5,50,0,0,70,32,0,70]
@@ -18,12 +21,42 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupGraph()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func setupGraph() {
+        let data: [Double] = [4, 8, 15, 16, 23, 42]
+        let labels = ["one", "two", "three", "four", "five", "six"]
+        
+        self.graphView.backgroundFillColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
+        
+        self.graphView.lineWidth = 1
+        self.graphView.lineColor = UIColor(red: 119/255, green: 119/255, blue: 119/255, alpha: 1)
+        self.graphView.lineStyle = ScrollableGraphViewLineStyle.smooth
+        
+        self.graphView.shouldFill = true
+        self.graphView.fillType = ScrollableGraphViewFillType.gradient
+        self.graphView.fillColor = UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1)
+        self.graphView.fillGradientType = ScrollableGraphViewGradientType.linear
+        self.graphView.fillGradientStartColor = UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1)
+        self.graphView.fillGradientEndColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 1)
+        
+        self.graphView.dataPointSpacing = 80
+        self.graphView.dataPointSize = 2
+        self.graphView.dataPointFillColor = UIColor.white
+        
+        self.graphView.referenceLineLabelFont = UIFont.boldSystemFont(ofSize: 8)
+        self.graphView.referenceLineColor = UIColor.white.withAlphaComponent(0.2)
+        self.graphView.referenceLineLabelColor = UIColor.white
+        self.graphView.dataPointLabelColor = UIColor.white.withAlphaComponent(0.5)
+        
+        self.graphView.set(data: data, withLabels: labels)
     }
     
     // Table View
